b31908b91f0389320976f3ba3d52c6574725b7fb
kevin guan <guanxiaochao@gmail.com>
2015-05-16 19:11:43 +0800

    v2

--- app/src/main/java/com/example/gxc/howold/FaceppDetect.java
+++ app/src/main/java/com/example/gxc/howold/FaceppDetect.java
@@ -1,21 +1,70 @@
 package com.example.gxc.howold;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
+import com.facepp.error.FaceppParseException;
+import com.facepp.http.HttpRequests;
+import com.facepp.http.PostParameters;
+
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpRequest;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.RequestLine;
+import org.apache.http.params.HttpParams;
 import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by gxc on 2015/5/16.
  */
 public class FaceppDetect {
 
-     public interface Callback
-     {
-         void success (JSONObject result);
-         void error(PacdPl);
-     }
-     public static void detect(Bitmap bm)
-     {
+    public interface Callback {
+        void success(JSONObject result);
 
-     }
+        void error(FaceppParseException exception);
+    }
+
+    public static void detect(final Bitmap bm, final Callback callBack) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    //request
+                    HttpRequests requests = new HttpRequests(Constant.KEY, Constant.SECRET, true, true);
+                    Bitmap bmSmall = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight());
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bmSmall.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+                    byte[] arrays = stream.toByteArray();
+
+                    PostParameters params = new PostParameters();
+                    params.setImg(arrays);
+                    JSONObject jsonObject =  requests.detectionDetect(params);
+
+                    Log.e("TAG", jsonObject.toString());
+
+                    if(callBack!=null)
+                    {
+                        callBack.success(jsonObject);
+                    }
+
+
+                } catch (FaceppParseException e) {
+                    e.printStackTrace();
+
+                    if(callBack != null){
+                        callBack.error(e);
+                    }
+                }
+            }
+
+        }).start();
+
+    }
 }
--- app/src/main/java/com/example/gxc/howold/MainActivity.java
+++ app/src/main/java/com/example/gxc/howold/MainActivity.java
@@ -5,27 +5,35 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facepp.error.FaceppParseException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
-    private static final int PICK_CODE =  0X110;
+    private static final int PICK_CODE = 0X110;
     private ImageView mPhoto;
-    private Button  mGetImage;
+    private Button mGetImage;
     private Button mDetect;
     private TextView mTip;
     private View mWating;
 
-    private  Bitmap mPhotoImage;
+    private Bitmap mPhotoImage;
     private String mCurrentPhotoStr;
-
 
 
     @Override
@@ -42,9 +50,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     private void InitEvents() {
         mGetImage.setOnClickListener(this);
         mDetect.setOnClickListener(this);
-        ;
-    }
 
+    }
 
 
     private void initViews() {
@@ -54,11 +61,56 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         mTip = (TextView) findViewById(R.id.id_tip);
     }
 
+    private static final int MSG_SUCCESS = 0x111;
+    private static final int MSG_ERROR = 0x112;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SUCCESS:
+                    mWating.setVisibility(View.GONE);
+                    JSONObject rs = (JSONObject) msg.obj;
+                    prepareRsBitmap(rs);
+                    mPhoto.setImageBitmap(mPhotoImage);
+
+                    break;
+                case MSG_ERROR:
+                    mWating.setVisibility(View.GONE);
+                    String errorMsg = (String) msg.obj;
+                    if(TextUtils.isEmpty(errorMsg))
+                    {
+                        mTip.setText("Error");
+                    }else{
+                        mTip.setText(errorMsg);
+                    }
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    private void prepareRsBitmap(JSONObject rs) {
+
+        try {
+            JSONArray  faces =  rs.getJSONArray("face");
+            int faceCount = faces.length();
+            mTip.setText("find" + faceCount);
+            for(int i=0; i<faceCount; i++){
+                //拿到单独的face对象
+                JSONObject face = faces.getJSONObject(i);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public void onClick(View v) {
 
-        switch ( v.getId())
-        {
+        switch (v.getId()) {
             case R.id.id_getImage:
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
@@ -68,31 +120,48 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
             case R.id.id_detect:
 
+                mWating.setVisibility(View.VISIBLE);
+                FaceppDetect.detect(mPhotoImage, new FaceppDetect.Callback() {
+
+                    @Override
+                    public void success(JSONObject result) {
+                        Message msg = Message.obtain();
+                        msg.what = MSG_SUCCESS;
+                        msg.obj = result;
+                        mHandler.sendMessage(msg);
+                    }
+
+                    @Override
+                    public void error(FaceppParseException exception) {
+                        Message msg = Message.obtain();
+                        msg.what = MSG_ERROR;
+                        msg.obj = exception.getErrorMessage();
+                        mHandler.sendMessage(msg);
+                    }
+                });
+
                 break;
 
 
-
         }
-}
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 
-        if(requestCode == PICK_CODE)
-        {
-            if(intent != null)
-            {
-                 Uri uri  =   intent.getData();
-                 Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-                 cursor.moveToFirst();
+        if (requestCode == PICK_CODE) {
+            if (intent != null) {
+                Uri uri = intent.getData();
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                cursor.moveToFirst();
 
-                 int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-                 cursor.close();
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                cursor.close();
 
-                 resizePhoto();
+                resizePhoto();
 
-                 mPhoto.setImageBitmap(mPhotoImage);
-                 mTip.setText("Click Detect ==>");
+                mPhoto.setImageBitmap(mPhotoImage);
+                mTip.setText("Click Detect ==>");
             }
         }
 
@@ -105,12 +174,12 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         BitmapFactory.decodeFile(mCurrentPhotoStr, options);
 
-        double ratio = Math.max(options.outWidth *1.0d/ 1024f,  options.outHeight*1.0d/ 1024f);
+        double ratio = Math.max(options.outWidth * 1.0d / 1024f, options.outHeight * 1.0d / 1024f);
 
-        options.inSampleSize =  (int)Math.ceil(ratio);
+        options.inSampleSize = (int) Math.ceil(ratio);
 
         options.inJustDecodeBounds = false;
 
-        mPhotoImage =   BitmapFactory.decodeFile(mCurrentPhotoStr, options);
+        mPhotoImage = BitmapFactory.decodeFile(mCurrentPhotoStr, options);
     }
- }
+}
